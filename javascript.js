2121bc153a60ab7a97a6d2a9dd7b6b3d239302f6
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2025-05-18 18:20:03 -0500

    Write pseudocode

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
 console.log("Hello World! It's time for Rock Paper Scissors!")
+ 
+// Logic to get computer choice
+// Computer generates a random whole number greater than zero but no greater than 3
+// 1 = "rock", 2 = "paper", 3 = "scissors"
+// Store result in ComputerChoice
