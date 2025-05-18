34ff0e62a2ccc8e09b91b92de4611b9928b10d86
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2025-05-18 18:46:06 -0500

    Write and test computer choice function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,24 @@
 console.log("Hello World! It's time for Rock Paper Scissors!")
  
 // Logic to get computer choice
-// Computer generates a random whole number greater than zero but no greater than 3
 // 1 = "rock", 2 = "paper", 3 = "scissors"
 // Store result in ComputerChoice
+
+function getComputerChoice() {
+    let computerChoice = null;
+    let randomNumber = Math.floor(Math.random() * 3 + 1); // returns random whole number > 0 and <= 3
+    switch (randomNumber) {
+        case 1:
+            computerChoice = "rock";
+            break;
+        case 2:
+            computerChoice = "paper";
+            break;
+        case 3:
+            computerChoice = "scissors"
+            break;
+    }
+    return computerChoice;
+}
+ 
+console.log(getComputerChoice());
