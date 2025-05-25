dbeff971accd1225aa1ad570515e5db3d5f9471f
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2025-05-25 18:22:38 -0500

    Write pseudocode for playing a round.

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@ console.log("Hello World! It's time for Rock Paper Scissors!")
 let computerScore = 0;
 let humanScore = 0;
 
+console.log(computerScore + " " + humanScore);
+
 // Logic to get computer choice
 // Choices are: 1 = "rock", 2 = "paper", 3 = "scissors"
 // Store result in computerChoice
@@ -36,3 +38,31 @@ function getHumanChoice() {
 
 console.log(getHumanChoice());
 console.log(getComputerChoice());
+
+// Logic to play a single round.
+// Get human and computer choices.
+// If humanChoice = computerChoice, 
+//      declare a tie and do not increment score.
+// If humanChoice = 'rock' 
+//      If computerChoice = 'paper', 
+//          declare computer winner, 
+//          increment computerScore by 1.
+//      Else if computerChoice = 'scissors', 
+//          declare human winner, 
+//          increment humanScore by 1.
+// Else if humanChoice = 'paper'
+//      If computerChoice = 'scissors', 
+//          declare computer winner, 
+//          increment computreScore by 1.
+//      Else if computerChoice = 'rock', 
+//          declare human winner, 
+//          increment humanScore by 1.
+// Else if humanChoice = 'scissors'
+//      If computerChoice = 'rock', 
+//          declare computer winner, 
+//          increment computerScore by 1.
+//      Else if computerChoice = 'paper', 
+//      declare human winner, 
+//      increment humanScore by 1.
+
+
