cde8bc64f35c7ecb0c1f9a44e5fc81487b33f132
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2025-05-27 20:42:39 -0500

    Code logic for 5 rounds of game play

--- javascript.js
+++ javascript.js
@@ -36,8 +36,6 @@ function getHumanChoice() {
     return humanChoice.toLowerCase(); // converts user text to all lower case
 }
 
-function playGame() {
-    
 // Logic to play a single round.
 // Get human and computer choices.
 // If humanChoice = computerChoice, 
@@ -67,45 +65,63 @@ function playGame() {
 //          declare human winner, 
 //          increment humanScore by 1.
 
-function playRound(humanChoice, computerChoice) {
-  console.log('You chose: ' + humanChoice + '; Computer chose: ' + computerChoice);
-  if (humanChoice === computerChoice) {
-    console.log("It's a tie!");
-  } else if (humanChoice === 'rock') {
-        if (computerChoice === 'paper') {
-            ++computerScore;
-            console.log("Computer wins! Paper beats rock.");
-        } else if (computerChoice === 'scissors') {
-            ++humanScore;
-            console.log("You win! Rock beats scissors.");
-        }   
-  } else if (humanChoice === 'paper') {
-        if (computerChoice === 'scissors') {
-            ++computerScore;
-            console.log("Computer wins! Scissors beats paper.");
-        } else if (computerChoice === 'rock') {
-            ++humanScore;
-            console.log("You win! Paper beats rock.");
-        }   
-  } else if (humanChoice === 'scissors') {
-        if (computerChoice === 'rock') {
-            ++computerScore;
-            console.log("Computer wins! Rock beats scissors.");
-        } else if (computerChoice === 'paper') {
-            ++humanScore;
-            console.log("You win! Scissors beats paper.");
-        } 
-  }
-  console.log('Score - You: ' + humanScore + ', Computer: ' + computerScore);
-}  
-// These consts and playRound will be called from within playGame
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
 
-playRound(humanSelection, computerSelection);
+    function playRound(humanChoice, computerChoice) {
+      console.log('*** Round ' + roundCounter + ' ***'); // see roundCounter in loop after playRound
+      console.log('You chose: ' + humanChoice + '; Computer chose: ' + computerChoice);
+      if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+      } else if (humanChoice === 'rock') {
+            if (computerChoice === 'paper') {
+                ++computerScore;
+                console.log("Computer wins! Paper beats rock.");
+            } else if (computerChoice === 'scissors') {
+                ++humanScore;
+                console.log("You win! Rock beats scissors.");
+            }   
+      } else if (humanChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                ++computerScore;
+                console.log("Computer wins! Scissors beats paper.");
+            } else if (computerChoice === 'rock') {
+                ++humanScore;
+                console.log("You win! Paper beats rock.");
+            }   
+      } else if (humanChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                ++computerScore;
+                console.log("Computer wins! Rock beats scissors.");
+            } else if (computerChoice === 'paper') {
+                ++humanScore;
+                console.log("You win! Scissors beats paper.");
+            } 
+      }
+      console.log('Score - You: ' + humanScore + ', Computer: ' + computerScore);
+    }  
+
+    // These consts and playRound will be called from within playGame
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
 
 }
 
-// To play 5 rounds, loop playGame until humanScore OR computerScore = 5
-// Check to see who has the highest score then declare the winner.
+// To play 5 rounds, loop playGame 5x
 
+
+let roundCounter = 0; // roundCounter displayed in playRound.
+while (roundCounter < 5) {
+    roundCounter++; // this is the round counter
+    playGame();
+} 
+
+// Check to see who has the highest score then declare the winner.
+if (humanScore > computerScore) {
+    console.log('You win!');
+} else if (computerScore > humanScore) {
+    console.log('The computer wins.');
+} else {
+    console.log('Tie game!');
+}  
