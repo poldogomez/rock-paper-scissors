1a5b0ea03df7f178ecf3ddd423a775f9ca6f1e75
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2026-01-25 15:48:25 -0600

    Reposition declareWinner logic to display at 5 pts

--- javascript.js
+++ javascript.js
@@ -36,60 +36,59 @@ function getComputerChoice() {
     return computerChoice;
 };
 
-    function playRound(humanChoice, computerChoice) {
-      
-      if (humanScore == 5 || computerScore == 5) {
-        if (humanScore > computerScore) {
-            declareWinner.textContent = "You win the game!";
-            banner.appendChild(declareWinner);
-            // alert('You win the game!');
-        } else {
-            declareWinner.textContent = "The computer wins the game!";
-            banner.appendChild(declareWinner);
-            // alert('The computer wins the game.');
-        };
-      };
+function playRound(humanChoice, computerChoice) {
+    // if (humanScore == 5 || computerScore == 5) {
+    //     if (humanScore > computerScore) {
+    //         declareWinner.textContent = "You win the game!";
+    //         banner.appendChild(declareWinner);
+    //         // alert('You win the game!');
+    //     } else {
+    //         declareWinner.textContent = "The computer wins the game!";
+    //         banner.appendChild(declareWinner);
+    //         // alert('The computer wins the game.');
+    //     };
+    // };
 
-      displayRound.textContent = 'Round ' + roundCounter; // see roundCounter in loop after playRound
-      banner.appendChild(displayRound);
+    displayRound.textContent = 'Round ' + roundCounter; // see roundCounter in loop after playRound
+    banner.appendChild(displayRound);
       
-      displayChoices.textContent = 'You chose: ' + humanChoice + '; Computer chose: ' + computerChoice;
-      banner.appendChild(displayChoices);
+    displayChoices.textContent = 'You chose: ' + humanChoice + '; Computer chose: ' + computerChoice;
+    banner.appendChild(displayChoices);
       
-      if (humanChoice === computerChoice) {
+    if (humanChoice === computerChoice) {
         displayRoundWinner.textContent = "It's a tie!";
         banner.appendChild(displayRoundWinner);
-      } else if (humanChoice === 'rock') {
-            if (computerChoice === 'paper') {
-                ++computerScore;
-                displayRoundWinner.textContent = "Paper beats rock. Computer wins!";
-                banner.appendChild(displayRoundWinner);
-            } else if (computerChoice === 'scissors') {
-                ++humanScore;
-                displayRoundWinner.textContent = "Rock beats scissors. You win!";
-                banner.appendChild(displayRoundWinner);
-            }   
-      } else if (humanChoice === 'paper') {
-            if (computerChoice === 'scissors') {
-                ++computerScore;
-                displayRoundWinner.textContent = "Scissors beats paper. Computer wins!";
-                banner.appendChild(displayRoundWinner);
-            } else if (computerChoice === 'rock') {
-                ++humanScore;
-                displayRoundWinner.textContent = "Paper beats rock. You win!";
-                banner.appendChild(displayRoundWinner);
-            }   
-      } else if (humanChoice === 'scissors') {
-            if (computerChoice === 'rock') {
-                ++computerScore;
-                displayRoundWinner.textContent = "Rock beats scissors. Computer wins!";
-                banner.appendChild(displayRoundWinner);
-            } else if (computerChoice === 'paper') {
-                ++humanScore;
-                displayRoundWinner.textContent = "Scissors beats paper. You win!";
-                banner.appendChild(displayRoundWinner);
-            } 
-      }
+    } else if (humanChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            ++computerScore;
+            displayRoundWinner.textContent = "Paper beats rock. Computer wins!";
+            banner.appendChild(displayRoundWinner);
+        } else if (computerChoice === 'scissors') {
+            ++humanScore;
+            displayRoundWinner.textContent = "Rock beats scissors. You win!";
+            banner.appendChild(displayRoundWinner);
+        };   
+    } else if (humanChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            ++computerScore;
+            displayRoundWinner.textContent = "Scissors beats paper. Computer wins!";
+            banner.appendChild(displayRoundWinner);
+        } else if (computerChoice === 'rock') {
+            ++humanScore;
+            displayRoundWinner.textContent = "Paper beats rock. You win!";
+            banner.appendChild(displayRoundWinner);
+        };   
+    } else if (humanChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            ++computerScore;
+            displayRoundWinner.textContent = "Rock beats scissors. Computer wins!";
+            banner.appendChild(displayRoundWinner);
+        } else if (computerChoice === 'paper') {
+            ++humanScore;
+            displayRoundWinner.textContent = "Scissors beats paper. You win!";
+            banner.appendChild(displayRoundWinner);
+        }; 
+      };
       displayScore.textContent = 'Score - You: ' + humanScore + ', Computer: ' + computerScore;
       banner.appendChild(displayScore);
       
@@ -113,5 +112,18 @@ buttons.forEach((button) => {
     
     roundCounter++;
     playRound(humanChoice, computerChoice); // call function that plays game
+
+    if (humanScore == 5 || computerScore == 5) {
+        if (humanScore > computerScore) {
+            declareWinner.textContent = "You win the game!";
+            banner.appendChild(declareWinner);
+            // alert('You win the game!');
+        } else {
+            declareWinner.textContent = "The computer wins the game!";
+            banner.appendChild(declareWinner);
+            // alert('The computer wins the game.');
+        };
+    };
+
     });
 });
